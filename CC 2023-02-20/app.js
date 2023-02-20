a94f3690baaa560c08e05369e4c7b476cdf77448
udevkota <107569767+udevkota@users.noreply.github.com>
2023-02-20 13:59:51 -0500

    Add coding challenge for 02-20-2023

--- CC 2023-02-20/app.js
+++ CC 2023-02-20/app.js
@@ -1,6 +1,6 @@
 //Convert number to reversed array of digits 
 
-//Given a random non-negative number, you have to return the digits of this number within an array in reverse order. 
+//Given a random non-negative number, return the digits of this number within an array in reverse order. 
 
 //Example:
 // 348597 => [7,9,5,8,4,3]
